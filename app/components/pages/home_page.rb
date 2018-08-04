19aff3d89868df1b3ac482ce5af23b808170d61d
Barrie <barrie@tyger.com>
2018-08-04 11:47:15 +0100

    home page and overview image

--- app/components/pages/home_page.rb
+++ app/components/pages/home_page.rb
@@ -27,17 +27,26 @@ class HomePage < Hyperloop::Router::Component
             DIV(class: 'column') do
               DIV(class: 'introduction') do
                 H1(class: 'ui inverted header') do
-                  SPAN(class: 'library') { 'Hyperstack' }
+                  SPAN(class: 'library') { 'Build spectactular web applications with Ruby' }
                 end
+
+                DIV(class: 'ui hidden divider')
+                H2(class: 'ui inverted header') do
+                  SPAN(class: 'library') { 'Single-page apps without the pain of JavaScript' }
+                end
+
                 DIV(class: 'ui hidden divider')
                 Link('/start', class: 'ui big basic inverted pink view-ui button getstartedbutton') do
                   I(class: 'sidebar icon')
                   "\n          Get Started\n        "
                 end
+
               end
             end
           end
         end
+
+        Sem.Image(src: '/dist/images/overview.png')
 
         DIV(class: 'feature alternate ui vertical stripe segment') do
           DIV(class: 'ui three column equal height center aligned divided relaxed stackable page grid') do
@@ -49,6 +58,7 @@ class HomePage < Hyperloop::Router::Component
               P() { 'Build interactive Web applications quickly. Hyperloop encourages rapid development with clean, pragmatic design. With developer productivity as our highest goal, Hyperloop takes care of much of the hassle of Web development, so you can focus on innovation and delivering end-user value.' }
               A(href: '', class: 'ui secondary large button') { 'Simplicity' }
             end
+
             DIV(class: 'column') do
               H2(class: 'ui icon header') do
                 IMG(class: 'ui icon image', src: 'dist/images/icons/gear.png')
@@ -57,6 +67,7 @@ class HomePage < Hyperloop::Router::Component
               P() { 'One language. One model. One set of tests. The same business logic and domain models running on the clients and the server. Hyperloop is fully integrated with Rails and also gives you unfettered access to the complete universe of JavaScript libraries (including React) from within your Ruby code. Hyperloop lets you build beautiful interactive user interfaces in Ruby.' }
               A(href: '', class: 'ui pink large button hyperlooppinkbutton') { 'Isomorphic Ruby' }
             end
+
             DIV(class: 'column') do
               H2(class: 'ui icon header') do
                 IMG(class: 'ui icon image', src: 'dist/images/icons/plugin.png')
--- app/components/shared/app_menu.rb
+++ app/components/shared/app_menu.rb
@@ -1,9 +1,7 @@
 class AppMenu < Hyperloop::Router::Component
-
   param :section
 
   render do
-
     DIV(class: 'following bar fixed') do
       DIV(class: 'ui page grid') do
         DIV(class: 'column') do
@@ -12,17 +10,11 @@ class AppMenu < Hyperloop::Router::Component
             DIV(class: 'sides') do
               DIV(class: 'active learn side') do
                 IMG(class: 'ui image', src: 'dist/images/hyperloop-logo-small-pink.png')
-                #Link('/docs', class: 'item') { 'DOCUMENTATION' }
-
               end
-
             end
           end
 
           DIV(class: 'ui inverted right floated secondary menu') do
-            # DIV(class: 'item') do
-            #   IFRAME(class: 'github', src: 'http://ghbtns.com/github-btn.html?user=ruby-hyperloop&repo=hyperloop&type=watch&count=true', allowTransparency: 'true', frameBorder: '0', scrolling: '0', width: '100', height: '20')
-            # end
             DIV(class: 'ui language floating dropdown link item', id: 'languages') do
               I(class: 'world icon')
               DIV(class: 'text') { 'English' }
@@ -34,108 +26,31 @@ class AppMenu < Hyperloop::Router::Component
             A(class: 'view-ui item') do
               I(class: 'sidebar icon')
               " Menu\n        "
-            end.on(:click) {
-              SidebarStore.toggle
-            }
+            end.on(:click) { SidebarStore.toggle }
+
+            A(href: '/docs', 'data-site': 'ui', class: 'additional item visible') { 'Docs' }
             A(href: 'https://github.com/ruby-hyperloop', 'data-site': 'ui', class: 'additional item visible') { 'Github' }
-            A(href: 'https://gitter.im/ruby-hyperloop/chat', 'data-site': 'ui', class: 'additional item visible') { 'Gitter chat' }
+            A(href: 'https://gitter.im/ruby-hyperloop/chat', 'data-site': 'ui', class: 'additional item visible') { 'Chat' }
 
             if (params.section != 'home')
               Sem.MenuItem {
                 SiteSearch(section: params.section, history: history, location: location)
               }
-
               Sem.MenuItem {
-                #puts "MATCH #{location.pathname == "/searchresult"}"
                 if (!(SearchEngineStore.querystring.empty?) && !(location.pathname == "/searchresult"))
                   Sem.Button(color: 'pink') {"See results for: { #{SearchEngineStore.querystring} }"}.on(:click) do
                     params.history.push "/searchresult"
                   end
                 end
               }
-
             end
 
             Sem.MenuItem {
-              IFRAME(class: 'github', src: 'http://ghbtns.com/github-btn.html?user=ruby-hyperloop&repo=hyperloop&type=watch&count=true', frameBorder: '0', scrolling: '0', width: '100', height: '20')
+              IFRAME(class: 'github', src: 'http://ghbtns.com/github-btn.html?user=ruby-hyperloop&repo=hyper-react&type=watch&count=true', frameBorder: '0', scrolling: '0', width: '100', height: '20')
             }
-
           end
-
         end
       end
     end
-  #   Sem.Menu(fixed: :top, inverted: false) do
-  #     sidebar_toggle
-  #     home
-  #     start
-  #     installation
-  #     tutorials
-  #     gems
-  #     tools
-  #     docs
-  #     search
-  #   end
-  # end
-
-  # def sidebar_toggle
-  #   A(class: 'item', id: 'togglemenu') do
-  #     Sem.Icon(name: 'sidebar')
-  #   end.on(:click) {
-  #     SidebarStore.toggle
-  #   }
-  # end
-
-  # def home
-  #   Sem.MenuItem {
-  #     Link('/') { 'HOME' }
-  #   }
-  # end
-
-  # def start
-  #   Sem.MenuItem {
-  #     Link('/start') { 'START' }
-  #   }
-  # end
-
-  # def installation
-  #   Sem.MenuItem {
-  #     Link('/installation') { 'INSTALLATION' }
-  #   }
-  # end
-
-  # def tutorials
-  #   Sem.MenuItem {
-  #     Link('/tutorials') { 'TUTORIALS' }
-  #   }
-  # end
-
-  # def gems
-  #   Sem.MenuItem {
-  #     Link('/gems') { 'GEMS' }
-  #   }
-  # end
-
-  # def tools
-  #   Sem.MenuItem {
-  #     Link('/tools') { 'TOOLS' }
-  #   }
-  # end
-
-  # def docs
-  #   Sem.MenuItem {
-  #     Link('/docs') { 'DOCS' }
-  #   }
-  # end
-
-  # def search
-  #   Sem.MenuMenu(position: :right) {
-  #     Sem.MenuItem {
-  #       SiteSearch()
-  #     }
-  #   }
-  # end
-
   end
-
 end
