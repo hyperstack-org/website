efbed438f3e79b71147c770c70c9989479e44223
Barrie <barrie@tyger.com>
2018-08-11 10:01:40 +0100

    redirect_to_https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  before_action :redirect_to_https
+
+  def redirect_to_https
+    redirect_to protocol: 'https://' unless request.ssl? || request.local?
+  end
 end
