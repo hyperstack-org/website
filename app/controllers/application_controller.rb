48513e64259ccbf6b0c492f61d21cc536d5a3267
Johan Smits <johan@smitsmail.net>
2018-08-12 20:41:10 +0200

    Revert "redirect_to_https"
    This reverts commit efbed438f3e79b71147c770c70c9989479e44223.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
-
-  before_action :redirect_to_https
   before_action :redirect_from_www
-
-  def redirect_to_https
-    redirect_to protocol: 'https://' unless request.ssl? || request.local?
-  end
 
   def redirect_from_www
     redirect_to 'https://hyperstack.org' + request.fullpath, status: 301 if request.host == 'www.hyperstack.org'
   end
+
 end
