e70b9fe2bed3f34915eccd227e019e22766264a5
Johan Smits <johan@smitsmail.net>
2018-08-12 20:52:00 +0200

    Make the www migration generic and not hardcoded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :redirect_from_www
 
   def redirect_from_www
-    redirect_to 'https://hyperstack.org' + request.fullpath, status: 301 if request.host == 'www.hyperstack.org'
+    redirect_to request.original_url.gsub(/\A(?<scheme>http(?:s):\/\/)?(www\.)(?<url>.*)/,'{\k<scheme>\k<url>}'), status: 301 if request.host[/\Awww\./]
   end
 
 end
