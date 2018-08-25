bd5c48907ad76f955b7376e7cc51dd4ba6b1848b
Jan Biedermann <jan@kursator.de>
2018-08-25 19:34:48 +0200

    remove webpacker gem

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -4,8 +4,6 @@
 // // a relevant structure within app/javascript and only use these pack files to reference
 // // that code so it'll be compiled.
 // //
-// // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// // layout file, like app/views/layouts/application.html.erb
 //
 // console.log('Hello World from Webpacker')
 //
