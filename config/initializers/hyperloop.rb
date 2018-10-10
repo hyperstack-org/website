013feee1faef95992b175f44cdfc27e1a80d0934
Barrie <barrie@tyger.com>
2018-10-10 13:28:53 +0100

    99.3 gems

--- config/initializers/hyperloop.rb
+++ config/initializers/hyperloop.rb
@@ -5,4 +5,5 @@ Hyperloop.configuration do |config|
   config.import 'opal_hot_reloader'
   config.cancel_import 'react/react-source-browser' # bring your own React and ReactRouter via Yarn/Webpacker
   #config.console_auto_start = false
+  # config.import 'react/ext/opal-jquery/element', client_only: true
 end
