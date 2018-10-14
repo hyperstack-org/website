fd0b68ad9b0cb74351ba8a975b41078a1579fa7f
Barrie <barrie@tyger.com>
2018-10-14 12:49:01 +0100

    refactored

--- app/hyperloop/components/shared/live_code_segment.rb
+++ app/hyperloop/components/shared/live_code_segment.rb
@@ -1,33 +1,3 @@
-class CompiledJS < Hyperloop::Component
-  param :html
-
-  render do
-    params.html.render
-  end
-end
-
-class RenderComponent < Hyperloop::Component
-  param :component_name
-  param :random_key
-
-  before_update { @errors = false }
-
-  after_error do |error, info|
-    puts "error = #{error}"
-    puts "info = #{info}"
-    @errors = true
-   end
-
-  render do
-    begin
-      React.create_element( Module.const_get(params.component_name), {key: params.random_key}) unless @errors
-    rescue Exception => e
-      puts "caught it but cant do anything about it"
-      puts "error = #{e.message}"
-    end
-  end
-end
-
 class LiveCodeSegment < Hyperloop::Component
   param :content
   param :code
@@ -38,15 +8,15 @@ class LiveCodeSegment < Hyperloop::Component
 
   render(DIV, class: 'block') do
     Sem.Divider(hidden: true)
-
     Sem.Grid(columns: 2, relaxed: true, padded: false, container: false) do
       Sem.GridColumn(width: 4, className: 'tight-box') { params.content }
-      Sem.GridColumn(width: 12) do
+      Sem.GridColumn(width: 12, className: 'tight-box') do
         Sem.Container(className: 'white-background outline-box') do
-          tab
+          tabs
         end
       end
     end
+    Sem.Divider(hidden: true)
   end
 
   def code_editor_and_results
@@ -63,14 +33,15 @@ class LiveCodeSegment < Hyperloop::Component
     end
   end
 
-  def tab
+  def tabs
     live_code = code_editor_and_results.as_node
-    compiled_js = CompiledJS(html: opal_code_html.as_node).as_node
+    compiled_js = CompiledJsTab(opal_code: @compiled_code).as_node
 
     panes = []
     panes.concat [ { menuItem: 'Live Ruby',   render: -> { live_code.to_n } },
-                   { menuItem: 'JavaScript', render: -> { compiled_js.to_n } }
+                   { menuItem: 'Generated JavaScript', render: -> { compiled_js.to_n } }
     ]
+
     menu = { secondary: false, pointing: true }
     Sem.Tab(menu: menu.to_n, panes: panes.to_n )
   end
@@ -84,22 +55,6 @@ class LiveCodeSegment < Hyperloop::Component
       matchBrackets: true
     }
     ReactCodeMirror(options: options.to_n, value: state.ruby_code.to_n, onChange: lambda { |value| mutate.ruby_code value })
-  end
-
-  def opal_code_html
-    if @compiled_code
-      html_code = `hljs.highlightAuto(#{@compiled_code}).value`
-      PRE(class: 'code pre-md-code') do
-        CODE(class: 'lang-javascript hljs small-code-font') do
-           DIV( dangerously_set_inner_HTML: { __html: html_code } )
-         end
-      end
-    else
-      Sem.Message(negative: true) do
-        H3 { state.compile_error_heading }
-        P { state.compile_error_message }
-      end
-    end
   end
 
   def compile
