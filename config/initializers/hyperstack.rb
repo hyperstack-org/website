d086812588e614c0c7191da9096b43a945d353b3
Barrie <barrie@tyger.com>
2018-11-18 17:46:26 +0000

    tidy

--- config/initializers/hyperstack.rb
+++ config/initializers/hyperstack.rb
@@ -1,13 +1,3 @@
-# Hyperstack.configuration do |config|
-#   # helper = Webpacker::Helper
-#   # config.transport = :action_cable
-#   # config.import 'reactrb/auto-import'
-#   config.import 'opal_hot_reloader'
-#   config.cancel_import 'react/react-source-browser' # bring your own React and ReactRouter via Yarn/Webpacker
-#   #config.console_auto_start = false
-#   # config.import 'react/ext/opal-jquery/element', client_only: true
-# end
-
 Hyperstack.configuration do |config|
   config.prerendering = :off
 
