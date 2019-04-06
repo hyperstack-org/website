b24cd5e59efdea48c5177a4689746ca4838edad5
Barrie <barrie@tyger.com>
2019-04-06 13:10:18 +0100

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'hyperstack#AppRouter'
   mount Hyperstack::Engine => '/hyperstack'
   get '/local_docs' => 'local_docs#index'
-  get '/question' => 'question#index'
+  get '/question' => 'question#question'
   get '/questions' => 'question#questions'
   get '/slack' => 'slack#index'
   get '/slack-invite' => 'slack#invite'
